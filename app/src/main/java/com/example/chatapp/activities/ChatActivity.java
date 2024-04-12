68aac4cfdfc90d0514ec5e342af78a8888a95a7f
NamPhungNguyen <namhien12082003@gmail.com>
2024-04-12 21:34:26 +0700

    recent conversation

--- app/src/main/java/com/example/chatapp/activities/ChatActivity.java
+++ app/src/main/java/com/example/chatapp/activities/ChatActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.chatapp.adapters.ChatAdapter;
@@ -14,7 +15,11 @@ import com.example.chatapp.models.ChatMessage;
 import com.example.chatapp.models.User;
 import com.example.chatapp.utilities.Constants;
 import com.example.chatapp.utilities.PreferenceManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -34,6 +39,7 @@ public class ChatActivity extends AppCompatActivity {
     private ChatAdapter chatAdapter;
     private PreferenceManager preferenceManager;
     private FirebaseFirestore databse;
+    private String conversionId = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,16 +68,32 @@ public class ChatActivity extends AppCompatActivity {
         databse = FirebaseFirestore.getInstance();
     }
     private void sendMessage(){
+        //tạo một bản ghi mới trong firebase chứa thông tin về tin nhắn
         HashMap<String, Object> message = new HashMap<>();
         message.put(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID));
         message.put(Constants.KEY_RECEIVER_ID, receivedUser.id);
         message.put(Constants.KEY_MESSAGE, binding.inputMessage.getText().toString());
         message.put(Constants.KEY_TIMESTAMP, new Date());
         databse.collection(Constants.KEY_COLLECTION_CHAT).add(message);
+        if (conversionId != null){
+            updateConversion(binding.inputMessage.getText().toString());
+        }else {
+            HashMap<String, Object> conversion = new HashMap<>();
+            conversion.put(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID));
+            conversion.put(Constants.KEY_SENDER_NAME, preferenceManager.getString(Constants.KEY_NAME));
+            conversion.put(Constants.KEY_SENDER_IMAGE, preferenceManager.getString(Constants.KEY_IMAGE));
+            conversion.put(Constants.KEY_RECEIVER_ID, receivedUser.id);
+            conversion.put(Constants.KEY_RECEIVER_NAME, receivedUser.name);
+            conversion.put(Constants.KEY_RECEIVER_IMAGE, receivedUser.image);
+            conversion.put(Constants.KEY_LAST_MESSAGE, binding.inputMessage.getText().toString());
+            conversion.put(Constants.KEY_TIMESTAMP, new Date());
+            addConversion(conversion);
+        }
         binding.inputMessage.setText(null);
     }
 
     private void listenMessage(){
+        // lắng nghe sự kiện để nhận tin nhắn từ firebase và cập nhật giao diện
         databse.collection(Constants.KEY_COLLECTION_CHAT)
                 .whereEqualTo(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
                 .whereEqualTo(Constants.KEY_RECEIVER_ID, receivedUser.id)
@@ -109,19 +131,76 @@ public class ChatActivity extends AppCompatActivity {
           binding.chatRecyclerView.setVisibility(View.VISIBLE);
       }
       binding.progressBar.setVisibility(View.GONE);
+      if (conversionId == null){
+          checkForConversion();
+      }
     };
     private Bitmap getBitmapFromEncodedString(String encodedImage){
         byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
     }
     private void loadReceiverDetails(){
+        //lấy thông tin về nguoi nhan tu intent và show name ra giao diện
         receivedUser = (User) getIntent().getSerializableExtra(Constants.KEY_USER);
         binding.textName.setText(receivedUser.name);
     }
-
     private String getReableDateTime(Date date){
         return new SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date);
     }
 
+    private void addConversion(HashMap<String, Object> conversion){
+        databse.collection(Constants.KEY_COLLECTION_CONVERSATIONS)
+                .add(conversion)
+                .addOnSuccessListener(documentReference -> conversionId = documentReference.getId());
+    }
 
+
+    private void updateConversion(String message){
+        DocumentReference documentReference =
+                databse.collection(Constants.KEY_COLLECTION_CONVERSATIONS)
+                        .document(conversionId);
+        documentReference.update(
+                Constants.KEY_LAST_MESSAGE, message,
+                Constants.KEY_TIMESTAMP, new Date()
+        );
+    }
+
+    private void checkForConversion(){
+        if (chatMessages.size() != 0){
+            checkForConversionRemotely(
+                    preferenceManager.getString(Constants.KEY_USER_ID),
+                    receivedUser.id
+            );
+            checkForConversionRemotely(
+                    receivedUser.id,
+                    preferenceManager.getString(Constants.KEY_USER_ID)
+            );
+        }
+    }
+
+    private void checkForConversionRemotely(String senderId, String receiverId){
+        databse.collection(Constants.KEY_COLLECTION_CONVERSATIONS)
+                .whereEqualTo(Constants.KEY_SENDER_ID, senderId)
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, receiverId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() > 0){
+                            DocumentSnapshot documentSnapsshot = task.getResult().getDocuments().get(0);
+                            conversionId = documentSnapsshot.getId();
+                        }
+                    }
+                });
+    }
+
+//    private final OnCompleteListener<QuerySnapshot> conversationCompleteListener = new OnCompleteListener<QuerySnapshot>() {
+//        @Override
+//        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//            if (task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() > 0){
+//                DocumentSnapshot documentSnapsshot = task.getResult().getDocuments().get(0);
+//                conversionId = documentSnapsshot.getId();
+//            }
+//        }
+//    };
 }
